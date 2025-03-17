298f9029fff25ecf3b0202eb84bcb9301b0757ff
Elias Jacob <contato@eliasjacob.com.br>
2025-03-17 14:44:01 -0300

    Update dependencies and improve installation script for CUDA and CPU support

--- create_env.sh
+++ create_env.sh
@@ -3,8 +3,8 @@
 # Check for NVIDIA GPU
 if lspci | grep -iq nvidia; then
     echo "NVIDIA GPU found. Installing with CUDA support."
-    poetry install --sync --extras=cuda --with cuda
+    poetry install --extras=cuda --with cuda
 else
     echo "No NVIDIA GPU found. Installing with CPU support."
-    poetry install --sync --extras=cpu
+    poetry install --extras=cpu
 fi
